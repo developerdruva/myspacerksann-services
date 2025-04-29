33fbedbaae52cb6b7e983e0467c93ef139b6b283
Rajesh <developer.rajeshnuz@gmail.com>
2025-04-29 22:07:16 +0530

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const UserAccountRoutes = require('./routes/account.user.routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors());
 
 app.get('/', (webReq, webRes) => {
     console.log('welcome this is myspace rksann application running.');
