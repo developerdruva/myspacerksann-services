2378a60a2aafb80e3cd57affaffacfa8ca65009a
Rajesh <developer.rajeshnuz@gmail.com>
2025-04-29 22:24:05 +0530

    cmt

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: "https://myspace-rajesh.netlify.app",
-    credentials: true
+    // credentials: true
 }));
 
 app.get('/', (webReq, webRes) => {
