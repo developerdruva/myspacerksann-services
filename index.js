fe042cac7e2870ba63037faf9e500a5155c379b4
rsannala <rsannala@lululemon.com>
2025-06-09 15:03:51 +0530

    index changes

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const corsOptions = require("./utils/corsOptions");
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Enable pre-flight requests for all routes
+app.use(cors());
+// app.options("*", cors(corsOptions)); // Enable pre-flight requests for all routes
 
 app.get("/", (webReq, webRes) => {
   console.log("welcome this is myspace rksann application running.");
