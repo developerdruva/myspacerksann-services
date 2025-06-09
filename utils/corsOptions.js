cb0d985f5a9cc69dde1171c2d253e292e83000f5
rsannala <rsannala@lululemon.com>
2025-06-09 13:33:43 +0530

    Remove origin configuration from CORS options

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,10 +1,5 @@
 const corsOptions = {
-  origin: [
-    "*",
-    "https://myspace-rajesh.netlify.app",
-    "http://localhost:3000",
-    "http://localhost:3001",
-  ],
+  options: true,
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow cookies
