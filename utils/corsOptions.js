074cd04d02bb59d951fbf21c67a143224b13887a
rsannala <rsannala@lululemon.com>
2025-06-09 14:50:18 +0530

    cors options changed

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,8 +1,21 @@
+const allowedOrigins = [
+  "https://myspace-rajesh.netlify.app",
+  "http://localhost:3000",
+  "http://localhost:3001",
+];
+
 const corsOptions = {
-  options: true,
-  methods: ["GET", "POST"],
+  origin: function (origin, callback) {
+    // Allow if origin is in allowed list or if it's undefined (like in Postman or curl)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // Allow cookies
+  credentials: true,
 };
 
 module.exports = corsOptions;
