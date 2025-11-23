6256bae1e96f28e41874370f29296961ba3344cd
Rajesh <developer.rajeshnuz@gmail.com>
2025-11-23 17:08:27 +0530

    alignemnts

--- src/routes/account.user.routes.js
+++ src/routes/account.user.routes.js
@@ -1,30 +1,30 @@
-// var express = require("express");
-// var router = express.Router();
-// var userAccountController = require("../controllers/user.accounts/registration.controller");
-// const {
-//   uploadS3bucketMiddleware,
-// } = require("./middlewares/personDetailsMiddlewares/persondetails.middleware");
-// const uploadToBucket = uploadS3bucketMiddleware();
+var express = require("express");
+var router = express.Router();
+var userAccountController = require("../controllers/user.accounts/registration.controller");
+const {
+  uploadS3bucketMiddleware,
+} = require("./middlewares/personDetailsMiddlewares/persondetails.middleware");
+const uploadToBucket = uploadS3bucketMiddleware();
 
-// router.post("/chatbotusercreate", userAccountController?.registerUser);
-// router.post("/chatbotuserlogin", userAccountController?.loginUser);
-// router.post("/loginSimple", userAccountController.loginSimple);
-// router.post(
-//   "/saveprofiledetails",
-//   uploadToBucket.fields([
-//     { name: "profile_pic", maxCount: 1 },
-//     { name: "resume", maxCount: 1 },
-//   ]),
-//   userAccountController.saveProfileDetails
-// );
+router.post("/chatbotusercreate", userAccountController?.registerUser);
+router.post("/chatbotuserlogin", userAccountController?.loginUser);
+router.post("/loginSimple", userAccountController.loginSimple);
+router.post(
+  "/saveprofiledetails",
+  uploadToBucket.fields([
+    { name: "profile_pic", maxCount: 1 },
+    { name: "resume", maxCount: 1 },
+  ]),
+  userAccountController.saveProfileDetails
+);
 
-// module.exports = router;
+module.exports = router;
 
-// // const ds = multer.diskStorage({
-// //     destination: 'uploads',
-// //     filename: (req, file, cb)=>{
-// //         cb(null, Date.now()+'-'+file.originalname);
-// //     }
-// // })
+// const ds = multer.diskStorage({
+//     destination: 'uploads',
+//     filename: (req, file, cb)=>{
+//         cb(null, Date.now()+'-'+file.originalname);
+//     }
+// })
 
-// // const multerStorage = multer({storage: ds});
+// const multerStorage = multer({storage: ds});
--- src/routes/sample.data.routes.js
+++ src/routes/sample.data.routes.js
@@ -1,11 +1,11 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require("express");
+var router = express.Router();
 
-// var sampledataController = require('../controllers/samples/sampledata.controller');
+var sampledataController = require("../controllers/samples/sampledata.controller");
 
-// router.get('/getSampleRecord', sampledataController.getSampleRecord);
-// router.get('/dbrequestAllEmp', sampledataController.getEmployees);
-// router.get('/dbrequestEmpby/:id', sampledataController.getEmployeeById);
-// router.post('/createUser', sampledataController.insertEmployee);
+router.get("/getSampleRecord", sampledataController.getSampleRecord);
+router.get("/dbrequestAllEmp", sampledataController.getEmployees);
+router.get("/dbrequestEmpby/:id", sampledataController.getEmployeeById);
+router.post("/createUser", sampledataController.insertEmployee);
 
-// module.exports = router;
+module.exports = router;
